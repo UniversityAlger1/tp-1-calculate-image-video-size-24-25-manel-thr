8fc14c8cc9f3eae7b9a685269f87aaa5d476454e
manel-thr <154762658+manel-thr@users.noreply.github.com>
2024-11-22 11:11:01 +0100

    update1

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -1,13 +1,13 @@
-#include "config/grayScaleImage.h"
+#include <stdio.h>
 // Parameters:
 //   w: width of the image
 //   h: height of the image
 // Return value
 //   grayScale size of the image Bitmap (in byte)
-float grayScaleImage(int w, int h) {
+long long grayScaleImageSizeBits(int w, int h) {
    // YOUR CODE HERE - BEGIN
-   if (w > 0 || h > 0) {
-        return w * h; 
+   if (w > 0 && h > 0) {
+        return w * h *8; 
     }
 
    // YOUR CODE HERE - END
@@ -24,11 +24,30 @@ int main() {
     scanf("%d", &height);
 
     
-    int size = grayScaleImageSize(width, height);
+    long long sizeInBits = grayScaleImageSizeBits(width, height);
 
    
     if (size > 0) {
-        printf("The size of the grayscale image is: %d bytes\n", size);
+        printf("The size of the grayscale image is: \n");
+        long long sizeInBytes = sizeInBits / 8;
+
+        
+        if (sizeInBytes < 1024) {
+           
+            printf("  %lld bytes\n", sizeInBytes);
+        } else if (sizeInBytes < 1024 * 1024) {
+           
+            double sizeInKB = sizeInBytes / 1024.0; 
+            printf("  %.2f KB\n", sizeInKB);
+        } else if (sizeInBytes < 1024 * 1024 * 1024) {
+            
+            double sizeInMB = sizeInBytes / (1024.0 * 1024); 
+            printf("  %.2f MB\n", sizeInMB);
+        } else {
+           
+            double sizeInGB = sizeInBytes / (1024.0 * 1024 * 1024); 
+            printf("  %.2f GB\n", sizeInGB);
+        }
     } else {
         printf("Invalid dimensions provided. Please enter positive values.\n");
     }
